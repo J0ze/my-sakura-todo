d2ad1f6af882cf728782d196dd51b82ea7f25108
J0ze <3431551044@qq.com>
2025-11-22 16:29:20 +0800

    添加：tailwindcss样式

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. 引入插件
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 2. 使用插件
+  ],
 })
